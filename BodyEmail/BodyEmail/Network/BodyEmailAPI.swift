4539e7fd7aacef06a165670612cc4322af7e6014
Jordi Milla Catalán <jb.90@hotmail.es>
2020-12-15 17:52:54 +0100

    Create Network.

---
+++ BodyEmail/BodyEmail/Network/BodyEmailAPI.swift
@@ -0,0 +1,60 @@
+//
+//  BodyEmailAPI.swift
+//  BodyEmail
+//
+//  Created by Jordi Milla on 15/12/2020.
+//  Copyright © 2020 Jordi Milla Catalan. All rights reserved.
+//
+
+import Foundation
+import Moya
+
+enum BodyEmailAPI {
+    
+    case getBodyEmail
+}
+
+extension BodyEmailAPI:TargetType,AccessTokenAuthorizable {
+    public var baseURL: URL { return URL(string: "https://gorest.co.in/public-api")!}
+    
+    public var path: String {
+        switch self {
+        case .getBodyEmail:
+            return "comments"
+        }
+    }
+    
+    var method: Moya.Method {
+        switch self {
+        case .getBodyEmail:
+            return .get
+        }
+    }
+    
+    var authorizationType: AuthorizationType {
+        switch self {
+        case .getBodyEmail:
+            return .bearer
+        }
+    }
+    
+    public var task: Task {
+        switch self {
+        case .getBodyEmail:
+            return .requestPlain
+        }
+    }
+    
+    public var validationType: ValidationType {
+        return .none
+    }
+    
+    public var sampleData: Data {
+        return "".data(using: String.Encoding.utf8)!
+    }
+    
+    public var headers: [String : String]? {
+        return nil
+    }
+    
+}
